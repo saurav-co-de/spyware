b92d556499484fe8795db820b8f914e55503d786
saurav kumar <158753499+saurav-co-de@users.noreply.github.com>
2024-04-14 12:41:08 +0530

    Update project spyware.py

--- project spyware.py
+++ project spyware.py
@@ -145,9 +145,4 @@ if __name__ == "__main__":
    
 
 
-for i in range(10):
-    print("I will delete myself in " + str(10 - i) + " seconds.")
-    time.sleep(1)
 
-os.remove(__file__)
-print("Goodbye")
